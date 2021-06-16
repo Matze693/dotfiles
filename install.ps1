066bfecc485c29501b6a490d082fec636168ee6d
Matthias Schmidt <matthias.schmidt@endress.com>
2021-06-16 13:39:42 +0200

    optimize install script for powershell

--- install.ps1
+++ install.ps1
@@ -11,10 +11,7 @@ git -C $DOTBOT_DIR submodule sync --quiet --recursive
 git submodule update --init --recursive $DOTBOT_DIR
 
 foreach ($PYTHON in ('python', 'python3', 'python2')) {
-    # Python redirects to Microsoft Store in Windows 10 when not installed
-    if (& { $ErrorActionPreference = "SilentlyContinue"
-            ![string]::IsNullOrEmpty((&$PYTHON -V))
-            $ErrorActionPreference = "Stop" }) {
+    if (Get-Command $PYTHON -ErrorAction Ignore) {
         &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
         return
     }
